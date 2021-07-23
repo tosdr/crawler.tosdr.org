b2f5878ac7e381d495bbcaa819118dc27e61e615
Justin René Back <justin.back@tosdr.org>
2021-07-23 20:42:13 +0200

    Add Sentry to Crawlers.

--- crawler.js
+++ crawler.js
@@ -1,51 +1,74 @@
 const http = require('http');
 const dotenv = require('dotenv');
+const uuid = require('uuid');
 const color = require('chalk');
 const url = require('url');
 const functions = require('./functions/index');
 const package = require('./package');
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
 
 const axios = require('axios');
 dotenv.config();
 
+if(process.env.SENTRY_DSN) {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        tracesSampleRate: 1.0,
+    });
+}
+
 const httpserver = http.createServer((req, res) => {
 
+
     const query = url.parse(req.url, true).query;
-    res.writeHead(200, { 'content-type': 'application/json' })
     let data = { "error": false, "message": { "name": null, "remoteStacktrace": null } }
     if (Object.keys(query).length === 0) {
         res.writeHead(302, {
             'Location': 'https://to.tosdr.org/bot'
         });
         res.end();
+        transaction.finish();
         return;
     }
+
+    let request_id = uuid.v4();
+
+
+    const transaction = Sentry.startTransaction({
+        op: "transaction",
+        name: "Document Crawl",
+    });
+
+    res.writeHead(200, { 'content-type': 'application/json', 'x-request-id': request_id });
+
     if (!query.url) {
         res.write(JSON.stringify(functions.response.error("RequestArgumentErr", "Required arguments missing")));
         res.end();
+        transaction.finish();
         return;
     }
     if (!query.apikey && !functions.envIsEmpty("API_KEY")) {
         res.write(JSON.stringify(functions.response.error("RequestArgumentErr", "Required arguments missing")));
         res.end();
+        transaction.finish();
         return;
     }
     if (query.xpath === "" || typeof query.xpath === 'undefined') {
         query.xpath = "//body";
     }
-    /*
-    if (query.apikey !== process.env.API_KEY && !functions.envIsEmpty("API_KEY")) {
-        res.write(JSON.stringify(functions.response.error("RequestArgumentErr", "Invalid API Key")));
-        res.end();
-        return;
-    }
-    */
 
     if (req.method !== "GET") {
         res.write(JSON.stringify(functions.response.error("RequestMethodErr", "Only GET is supported")));
         res.end();
+        transaction.finish();
         return;
     }
+
+    Sentry.setTag("request_id", request_id)
+    Sentry.setTag("crawler", process.env.CRAWLER ? process.env.CRAWLER : 'legacy');
+
 
 
     let UserAgent = `ToSDRCrawler/${package.version} (+https://tosdr.org)`;
@@ -62,10 +85,14 @@ const httpserver = http.createServer((req, res) => {
     }).then((response) => {
         console.log("Got api key");
         if (response.status === 403 || response.status === 401) {
+            Sentry.captureMessage(response.statusText);
+            transaction.finish();
             res.write(JSON.stringify(functions.response.error("ServerError", "Invalid Server API Key. Misconfigured")));
             res.end();
             return;
         } else if (response.status !== 200) {
+            Sentry.captureMessage(response.statusText);
+            transaction.finish();
             res.write(JSON.stringify(functions.response.error("ServerError", response.statusText)));
             res.end();
             return;
@@ -84,14 +111,24 @@ const httpserver = http.createServer((req, res) => {
 
 
         functions.crawl(query.url, query.xpath).then((response) => {
+            console.log("Crawled Document");
             res.write(JSON.stringify(functions.response.success(response)));
             res.end();
         }).catch((err) => {
+            console.log("Send Error to Sentry");
+            Sentry.captureException(err);
             res.write(JSON.stringify(functions.response.error(err.name, err.message)));
             res.end();
+        }).finally(() => {
+            console.log("Finished transaction");
+            transaction.finish();
         });
     }).catch((error) => {
-
+        console.log("Send Error to Sentry");
+        Sentry.captureException(error);
+    }).finally(() => {
+        console.log("Finished transaction");
+        transaction.finish();
     });
 
 });
